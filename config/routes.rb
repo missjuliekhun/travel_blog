f0ef5e260de12523632d2fe8aaaa4aab896843e5
yourUsername <your_email@example.com>
2017-01-08 21:12:21 -0500

    scaffold for articles and migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :articles
+
+  root 'welcome#index'
+
+  get 'home/index'
+
+  get 'welcome/index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
